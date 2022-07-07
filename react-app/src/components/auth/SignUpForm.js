2243bdd72ff63ae648cc3e59e1b2f5ccce3c1274
Dylan <d.peate63@gmail.com>
2022-07-07 15:18:51 +0000

    local commit

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -4,6 +4,7 @@ import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
 
 /* eslint-disable no-useless-escape */
+//made local branch
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
